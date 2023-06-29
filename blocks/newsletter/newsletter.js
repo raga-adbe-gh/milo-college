76755e7fbd3713403aa774e47d600eb1597107c7
Raghu A <raghu@raghus-mbp.macromedia.com>
2023-06-29 11:07:37 +0530

    .

--- blocks/newsletter/newsletter.js
+++ blocks/newsletter/newsletter.js
@@ -2,7 +2,7 @@ import { getLibs } from '../../scripts/utils.js';
 
 export default async function init(el) {
   const titles = {...el.querySelectorAll('h1, h2, h3, h4, h5, h6, p')};
-  const { createTag } = await import(`${getLibs()/utils/utils.js}`);
+  const { createTag } = await import(`${getLibs()}/utils/utils.js`);
   const memberList = createTag('ul', {});
   const resp = await fetch('/email-list.json');
   if (!resp.json) return;
@@ -12,4 +12,5 @@ export default async function init(el) {
     memberList.append(person);
   });
   el.append(memberList);
+
 }
